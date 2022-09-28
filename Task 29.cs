87c0d8cda8e2ca54617b175a00e3b3536c1408b4
Evgeniy <jenyasereb95@gmail.com>
2022-09-28 22:06:32 +0500

    29

---
+++ Task 29.cs
@@ -0,0 +1,24 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+void FillArray(int [] num )
+{
+ for (int i = 0; i < num.Length; i++)
+{
+    num[i] = new Random().Next(1 , 20);
+}
+
+}
+    
+void PrintArray(int[] num )
+{
+
+for (int i = 0; i < num.Length; i++)
+{
+    Console.Write(num[i] + " ");
+    
+}
+
+}
+int size = 8;
+int [] numbers =  new int [size];
+ FillArray(numbers);
+ PrintArray(numbers);
